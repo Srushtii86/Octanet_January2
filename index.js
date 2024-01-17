b8fae8bbb591a47c2c75ebe8938285a4425e9b6a
Srushtii86 <srushtikp86@gmail.com>
2024-01-17 19:35:34 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const TodoModel = require('./Models/Todo')
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+
+app.get('/get' , (req,res) =>{
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.put('/update/:id' , (req,res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndUpdate({_id:id},{done : true})
+    .then(result=> res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.delete('/delete/:id' , (req,res)=>{
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id:id})
+    .then(result=> res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.post('/add', async (req, res) => {
+    const task = req.body.task;
+    try {
+        const result = await TodoModel.create({ task: task });
+        console.log('Task added successfully:', result);
+        res.json(result);
+    } catch (err) {
+        console.error('Error adding task:', err);
+        res.json(err);
+    }
+});
+
+app.listen(3001,()=>{
+    console.log("Server Is Running")
+})
+
+//Created APIs
